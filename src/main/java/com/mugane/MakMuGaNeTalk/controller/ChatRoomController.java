0ad639010e7896c082045e0de6d7631998d16a2d
noel7781 <noel7781@naver.com>
2022-10-31 14:46:39 +0900

    fix: Controller 리턴타입 ResponseEntity로 수정 및 메소드마다 인자 받는 어노테이션 추가

--- src/main/java/com/mugane/MakMuGaNeTalk/controller/ChatRoomController.java
+++ src/main/java/com/mugane/MakMuGaNeTalk/controller/ChatRoomController.java
@@ -1,43 +1,51 @@
 package com.mugane.MakMuGaNeTalk.controller;
 
-import com.mugane.MakMuGaNeTalk.common.ApiResponse;
 import com.mugane.MakMuGaNeTalk.dto.request.ChatRoomListRequestDto;
 import com.mugane.MakMuGaNeTalk.dto.request.CreateChatRoomInvitationRequestDto;
 import com.mugane.MakMuGaNeTalk.dto.request.CreateChatRoomRequestDto;
 import com.mugane.MakMuGaNeTalk.dto.response.ChatRoomListResponseDto;
 import com.mugane.MakMuGaNeTalk.enums.ChatRoomType;
 import com.mugane.MakMuGaNeTalk.service.ChatRoomService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ChatRoomController {
+
     private final ChatRoomService chatRoomService;
 
     @GetMapping("/v1/chat-rooms")
-    public List<ChatRoomListResponseDto> getChatRoomList(
-            ChatRoomListRequestDto req
+    public ResponseEntity<List<ChatRoomListResponseDto>> getChatRoomList(
+        @RequestParam ChatRoomListRequestDto req
     ) {
 
-        return chatRoomService.getChatRoomList(req)
+        return ResponseEntity.status(HttpStatus.OK).body(
+            chatRoomService.getChatRoomList(req)
                 .stream()
                 .map(ChatRoomListResponseDto::of)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
     @PostMapping("/v1/chat-rooms")
-    public ApiResponse<?> createChatRoom(
-            CreateChatRoomRequestDto req
+    public ResponseEntity<?> createChatRoom(
+        @RequestBody CreateChatRoomRequestDto req
     ) {
+
+        log.info("req : " + req.toString());
 
         ChatRoomType chatRoomType = req.getChatRoomType();
 
@@ -55,18 +63,18 @@ public class ChatRoomController {
             }
         }
 
-        return ApiResponse.success();
+        return ResponseEntity.status(HttpStatus.OK).body();
     }
 
     @PostMapping("/v1/chat-room-invitations")
-    public ApiResponse<?> createChatRoom(
-            CreateChatRoomInvitationRequestDto req
+    public ResponseEntity<?> createChatRoom(
+        @RequestBody CreateChatRoomInvitationRequestDto req
     ) {
 
-        chatRoomService.createChatRoomInvitation(req.getHostUserId(), req.getGuestUserId(), req.getFirstMessage());
-        return ApiResponse.success();
+        chatRoomService.createChatRoomInvitation(req.getHostUserId(), req.getGuestUserId(),
+            req.getFirstMessage());
+        return ResponseEntity.status(HttpStatus.OK).body();
     }
-
 
 
 }
--- src/main/java/com/mugane/MakMuGaNeTalk/dto/request/CreateChatRoomRequestDto.java
+++ src/main/java/com/mugane/MakMuGaNeTalk/dto/request/CreateChatRoomRequestDto.java
@@ -1,11 +1,13 @@
 package com.mugane.MakMuGaNeTalk.dto.request;
 
 import com.mugane.MakMuGaNeTalk.enums.ChatRoomType;
-import lombok.Getter;
-
 import java.util.Set;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CreateChatRoomRequestDto {
 
     private Long userId; // TODO 추후 제거 (JWT토큰에서 정보 가져오기)
@@ -13,4 +15,14 @@ public class CreateChatRoomRequestDto {
     private Set<String> tagList;
     private ChatRoomType chatRoomType;
     private String password;
+
+    @Builder
+    CreateChatRoomRequestDto(Long userId, String title, Set<String> tagList,
+        ChatRoomType chatRoomType, String password) {
+        this.userId = userId;
+        this.title = title;
+        this.tagList = tagList;
+        this.chatRoomType = chatRoomType;
+        this.password = password;
+    }
 }
