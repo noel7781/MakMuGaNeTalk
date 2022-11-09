46b01b5c7b346b0a9bbb9b40611fe37f1ec374e5
noel7781 <noel7781@naver.com>
2022-11-09 22:39:21 +0900

    feat: main page icon 추가

--- frontend/src/routes/main.jsx
+++ frontend/src/routes/main.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import MessageIcon from "@mui/icons-material/Message";
+import GroupsIcon from "@mui/icons-material/Groups";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div>Logged IN</div>
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+        onClick={() => navigate("/my-chat")}
+      >
+        <label name="my-chat">My Chat</label>
+        <MessageIcon fontSize="large" />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+        onClick={() => navigate("/group-chat")}
+      >
+        <label name="group-chat">Group Chat</label>
+        <GroupsIcon fontSize="large" />
+      </div>
     </div>
   );
 };
