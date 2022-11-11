68332c03483f5fedcdab796bb3961a4b6fa7f3ee
noel7781 <noel7781@naver.com>
2022-11-11 10:52:01 +0900

    fix: Validation 중복 제거

--- src/main/java/com/mugane/MakMuGaNeTalk/controller/UserRestController.java
+++ src/main/java/com/mugane/MakMuGaNeTalk/controller/UserRestController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
