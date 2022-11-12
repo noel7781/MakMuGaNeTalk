8eb1d38806c59d58b547d0c44ef0cbe5430c3b0b
noel7781 <noel7781@naver.com>
2022-11-12 11:21:23 +0900

    feat: data.sql에 nickname항목 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (user_id, email, nickname, password)
+VALUES (1, 'first@gmail.com', 'first', 'password'),
+       (2, 'seconde@gmail.com', 'second', 'password'),
+       (3, 'third@gmail.com', 'third', 'password');
+
