22a3122c44ee69241fc66e93975789b4311d12de
noel7781 <noel7781@naver.com>
2022-12-23 10:55:50 +0900

    refactor: 배포용 코드 수정

--- src/main/java/com/mugane/MakMuGaNeTalk/config/WebSocketConfiguration.java
+++ src/main/java/com/mugane/MakMuGaNeTalk/config/WebSocketConfiguration.java
@@ -2,6 +2,7 @@ package com.mugane.MakMuGaNeTalk.config;
 
 import com.mugane.MakMuGaNeTalk.config.security.StompHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -15,6 +16,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     private final StompHandler stompHandler;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -26,7 +33,11 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableStompBrokerRelay("/topic");
+        registry.enableStompBrokerRelay("/topic")
+            .setRelayHost(host)
+            .setRelayPort(61613)
+            .setClientLogin(username)
+            .setClientPasscode(password);
     }
 
     @Override
