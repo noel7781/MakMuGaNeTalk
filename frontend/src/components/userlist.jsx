f16e7fadd74cb2a18872b8afffa84b6811b90654
noel7781 <noel7781@naver.com>
2022-12-28 20:05:15 +0900

    refactor: 주석삭제, CODEOWNERS 정리

--- frontend/src/components/userlist.jsx
+++ frontend/src/components/userlist.jsx
@@ -5,7 +5,7 @@ const UserList = ({ users }) => {
   return (
     <React.Fragment>
       <div className="title">사용자</div>
-      <ul className="ul">
+      <ul className="userlists">
         {users.map((user) => (
           <li className="userlist" key={user.id}>
             {user.nickname}
